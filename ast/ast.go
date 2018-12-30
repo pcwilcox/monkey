1bb48d154c0dbe33c1fb09a2c4f5f0cdf72acbe5
Pete Wilcox <petercwilcox@gmail.com>
2018-12-29 22:59:53 -0800

    starting on the actual parser now

--- ast/ast.go
+++ ast/ast.go
@@ -5,12 +5,15 @@
 package ast
 
 import (
+	"bytes"
+
 	"../token"
 )
 
 // Node is a node in the tree
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 // Statement is implemented by nodes representing statements
@@ -37,10 +40,73 @@ type LetStatement struct {
 	Value Expression
 }
 
+// ReturnStatement defines a return statement
+type ReturnStatement struct {
+	Token       token.Token // the 'return' token
+	ReturnValue Expression
+}
+
+// ExpressionStatement is all of the other types of statement
+type ExpressionStatement struct {
+	Token      token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+
+// TokenLiteral gives the token
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+// String is the stringer function
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
+}
+
+func (rs *ReturnStatement) statementNode() {}
+
+//TokenLiteral gives the token
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+// String is the stringer
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
 func (ls *LetStatement) statementNode() {}
 
 // TokenLiteral returns the token literal
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+// String is the stringer
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
 
 // Identifier represents function and variable names
 type Identifier struct {
@@ -53,10 +119,24 @@ func (i *Identifier) expressionNode() {}
 // TokenLiteral returns the literal
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 
+// String is the stringer function
+func (i *Identifier) String() string { return i.Value }
+
 // TokenLiteral returns the literal for the root
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
 	}
 	return ""
+}
+
+// String is the stringer function
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
 }
--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"../ast"
 	"../lexer"
 	"../token"
@@ -15,11 +17,15 @@ type Parser struct {
 	l         *lexer.Lexer
 	curToken  token.Token
 	peekToken token.Token
+	errors    []string
 }
 
 // New creates a new parser
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
@@ -35,5 +41,89 @@ func (p *Parser) nextToken() {
 
 // ParseProgram doesn't do anything at the moment
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for !p.peekTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: we're skipping the expressions until we encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// TODO: we're skipping the expressions until we encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	}
+	p.peekError(t)
+	return false
+}
+
+// Errors returns a slice of the errors
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }

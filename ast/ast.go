b5860621868b9cb63781f7a505060ef58eb2f1e9
Pete Wilcox <petercwilcox@gmail.com>
2018-12-30 14:52:07 -0800

    if-else parsing

--- ast/ast.go
+++ ast/ast.go
@@ -218,3 +218,55 @@ func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
 
 // String ...
 func (b *Boolean) String() string { return b.Token.Literal }
+
+// IfExpression is an if expression lol
+type IfExpression struct {
+	Token       token.Token // the 'if' token
+	Condition   Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
+}
+
+func (ie *IfExpression) expressionNode() {}
+
+// TokenLiteral ...
+func (ie *IfExpression) TokenLiteral() string { return ie.Token.Literal }
+
+// String ...
+func (ie *IfExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("if")
+	out.WriteString(ie.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(ie.Consequence.String())
+
+	if ie.Alternative != nil {
+		out.WriteString("else ")
+		out.WriteString(ie.Alternative.String())
+	}
+
+	return out.String()
+}
+
+// BlockStatement is a block
+type BlockStatement struct {
+	Token      token.Token // the { token
+	Statements []Statement
+}
+
+func (bs *BlockStatement) statementNode() {}
+
+// TokenLiteral ...
+func (bs *BlockStatement) TokenLiteral() string { return bs.Token.Literal }
+
+// String ...
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	for _, s := range bs.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
